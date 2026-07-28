77902722980c8a49e941a925d3c8d1ba22e35622
Ryan Lee <56053560+CocoaCommander@users.noreply.github.com>
2026-07-28 11:38:38 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        // track chars w array
+        const charCountS = new Array(26).fill(0);
+        const charCountT = new Array(26).fill(0);
+
+        for (let i = 0; i < s.length; i++) {
+            let idxOfS: number = s.charCodeAt(i) - "a".charCodeAt(0);
+            let idxOfT: number = t.charCodeAt(i) - "a".charCodeAt(0);
+
+            charCountS[idxOfS]++;
+            charCountT[idxOfT]++;
+        }
+
+        return charCountS.join() === charCountT.join();
+    }
+}
